dc8e7c39a153b2cc48834847dd8116f4ecf124f6
Cedric Lapi <lapicedric@gmail.com>
2024-03-11 21:41:02 +0100

    authentication!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {Routes, Route} from 'react-router-dom';
 import RegisterForm from './components/RegisterForm';
 import Dashboard from './components/Dashboard';
+import LoginForm from './components/LoginForm';
 
 function App() {
   
@@ -12,7 +13,8 @@ function App() {
     <h1>Hello world!</h1>
     <Routes>
       <Route path="/" element={<RegisterForm />} />
-      <Route path="/api/ideas" element={<Dashboard />} />
+      <Route path="/ideas" element={<Dashboard />} />
+      <Route path="/login" element={<LoginForm />} />
     </Routes>
     </>
   )
--- server/routes/idea.routes.js
+++ server/routes/idea.routes.js
@@ -1,8 +1,9 @@
 
 const IdeaController = require('../controller/idea.controller');
+const { authenticate } = require('../config/jwt.config');
  
 module.exports = app => {
-    app.get('/api/ideas', IdeaController.findAllIdeas);
+    app.get('/api/ideas', authenticate, IdeaController.findAllIdeas);
     app.get('/api/ideas/:id/details',  IdeaController.findOneSingleIdea);
     app.patch('/api/ideas/:id/edit',  IdeaController.updateExistingIdea);
     app.post('/api/ideas/new',  IdeaController.createNewIdea);
